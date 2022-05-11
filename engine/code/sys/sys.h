ae790e1b070246e63cd6f35405e0fdded9ad116d
s-markow <jsaetrum@hotmail.com>
2022-05-11 12:58:01 +0200

    Add files via upload

--- engine/code/sys/sys.h
+++ engine/code/sys/sys.h
@@ -16,10 +16,12 @@ void Sys_Quit( sysState_t *state );
 void *Sys_Alloc( size_t size, cbool aligned );
 void Sys_Free( void *ptr );
 void *Sys_MemCpy( void *dst, const void *src, size_t size );
+void *Sys_MemSet() void *str, int c, size_t n );
 
 cbool Sys_PumpMessages();
 
 void Sys_Sleep( unsigned long ms );
+double Sys_GetTime();
 
 // syscon.c
 void Sys_Printf( const char *fmt, ... );

4141e028a4743d628779c249f9dde42f10471820
s-markow <jsaetrum@hotmail.com>
2022-05-17 21:20:10 +0200

    Create string.c

---
+++ engine/code/core/string.c
@@ -0,0 +1,26 @@
+#include "string.h"
+
+size_t C_StrLen( const char* str )
+{
+  return 0;
+}
+
+char *C_StrDup( const char* str )
+{
+  return NULL;
+}
+
+int C_StrCmp( const char* str1, const char* str2 )
+{
+  return 0; 
+}
+
+char *C_StrCpy( char *dst, const char* src )
+{
+  return NULL;
+}
+
+char *C_StrnCpy( char *dst, const char* src, size_t n )
+{
+  return NULL;
+}

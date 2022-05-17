1a6899ee14cd6e0965edf3db30d7d915dbf13c45
s-markow <jsaetrum@hotmail.com>
2022-05-17 21:20:48 +0200

    Update string.h

--- engine/code/core/string.h
+++ engine/code/core/string.h
@@ -7,7 +7,7 @@
 
 CSAPI size_t C_StrLen( const char* str );
 CSAPI char *C_StrDup( const char* str );
-CSAPI int *C_StrCmp( const char* str1, const char* str2 );
+CSAPI int C_StrCmp( const char* str1, const char* str2 );
 CSAPI char *C_StrCpy( char *dst, const char* src );
 CSAPI char *C_StrnCpy( char *dst, const char* src, size_t n );
 
